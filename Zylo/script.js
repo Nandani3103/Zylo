361cc3db7063ec94d13dc77da1e813d63e1cb1dc
Nandani Sharma <141529229+Nandani3103@users.noreply.github.com>
2025-04-26 20:58:05 +0530

    Add files via upload

---
+++ Zylo/script.js
@@ -0,0 +1,57 @@
+const carousel = document.getElementById('carousel');
+
+
+function scrollLeft() {
+  carousel.scrollBy({
+    left: -320,
+    behavior: 'smooth'
+  });
+}
+
+function scrollRight() {
+  carousel.scrollBy({
+    left: 320,
+    behavior: 'smooth'
+  });
+}
+
+const slidingImages = document.getElementById('sliding-images');
+
+function scrollLeft() {
+  slidingImages.scrollBy({ left: -300, behavior: 'smooth' });
+}
+
+function scrollRight() {
+  slidingImages.scrollBy({ left: 300, behavior: 'smooth' });
+}
+
+const slidingImages2 = document.getElementById('sliding-images-2');
+
+function scrollLeft() {
+  slidingImages2.scrollBy({ left: -300, behavior: 'smooth' });
+}
+
+function scrollRight() {
+  slidingImages2.scrollBy({ left: 300, behavior: 'smooth' });
+}
+
+const slidingImages3 = document.getElementById('sliding-images-3');
+
+function scrollLeft() {
+  slidingImages3.scrollBy({ left: -300, behavior: 'smooth' });
+}
+
+function scrollRight() {
+  slidingImages3.scrollBy({ left: 300, behavior: 'smooth' });
+}
+
+
+
+  
+
+
+
+
+
+
+
